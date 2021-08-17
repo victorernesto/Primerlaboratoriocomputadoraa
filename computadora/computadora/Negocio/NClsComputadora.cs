73bffdb43b2b3b4ef586cce1962c31478e74bec3
victorernesto <88003634+victorernesto@users.noreply.github.com>
2021-08-16 22:37:17 -0600

    cambio2

--- computadora/computadora/Negocio/NClsComputadora.cs
+++ computadora/computadora/Negocio/NClsComputadora.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using computadora.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace computadora.Negocio
 {
     class NClscomputadora
     {
+        public String Encendiendo(Clscomputadora computadora) 
+        {
+            computadora.MemoriaRam = 8;
+            computadora.Procesador = "intel core !5 dual";
+
+            return "la computadora con memoria ran de"+ computadora.MemoriaRam +"gb y micro procesadora"+ computadora.Procesador+"esta encendida";
+        }
+
+
+
     }
+    
+}
+   
 }
