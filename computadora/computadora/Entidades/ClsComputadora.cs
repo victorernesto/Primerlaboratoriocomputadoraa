5d5ffd81d212c8556a4392fd8d81a27ec0e1fe6c
victorernesto <88003634+victorernesto@users.noreply.github.com>
2021-08-16 21:52:47 -0600

    cambio1

--- computadora/computadora/Entidades/ClsComputadora.cs
+++ computadora/computadora/Entidades/ClsComputadora.cs
@@ -18,5 +18,16 @@ namespace computadora.Entidades
         private String sistemaoperativo;
         private String pantalla;
         private String almacenamiento;
+
+        public int Dimenciones { get => dimenciones; set => dimenciones = value; }
+        public int MemoriaRam { get => memoriaRam; set => memoriaRam = value; }
+        public string Procesador { get => procesador; set => procesador = value; }
+        public string Mous { get => mous; set => mous = value; }
+        public string Grafica { get => grafica; set => grafica = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Resolucion { get => resolucion; set => resolucion = value; }
+        public string Sistemaoperativo { get => sistemaoperativo; set => sistemaoperativo = value; }
+        public string Pantalla { get => pantalla; set => pantalla = value; }
+        public string Almacenamiento { get => almacenamiento; set => almacenamiento = value; }
     }
 }
